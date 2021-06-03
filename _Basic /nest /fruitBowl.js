216dc8354b565587c0dbb738aab97047dc8672e4
wentungwen <wentungwen@gmail.com>
2021-06-03 21:42:30 +0800

    d3 singular obj

--- _Basic /nest /fruitBowl.js
+++ _Basic /nest /fruitBowl.js
@@ -8,41 +8,45 @@ const radiusScale = d3
   .domain(["apple", "lemon"])
   .range([30, 50]);
 
-const xPosition = (d, i) => i * 100 + 60;
 export const fruitBowl = (selection, props) => {
   const { fruits, height } = props;
+
+  const bowl = selection
+    .selectAll("rect")
+    .data([null])
+    .enter()
+    .append("rect")
+    .attr("y", 110)
+    .attr("width", 920)
+    .attr("height", 300)
+    .attr("rx", 150);
 
   // group
   const groups = selection.selectAll("g").data(fruits, (d) => d.id);
   const groupEnter = groups.enter().append("g");
+
+  console.log(groups);
+  console.log(groupEnter);
+
   groupEnter
     .merge(groups)
-    .attr("transform", (d, i) => `translate${i * 100 + 60},${height / 2}`);
+    .attr("transform", (d, i) => `translate(${i * 100 + 80},${height / 2})`);
+
   groups.exit().remove();
 
   //circle
-  const circles = groups.select("circle");
   groupEnter
-    .enter()
     .append("circle")
-    .attr("cx", xPosition)
-    .attr("cy", height / 2)
-    .merge(circles)
+    .merge(groups.select("circle"))
     .transition()
     .duration(500)
     .attr("r", (d) => radiusScale(d.type))
-    .attr("cx", xPosition)
     .attr("fill", (d) => colorScale(d.type));
-  circles.exit().transition().duration(500).attr("r", 0).remove();
 
   // text
-  const texts = groups.selectAll("text").data(fruits, (d) => d.id);
-  texts
-    .enter()
+  groupEnter
     .append("text")
-    .attr("x", xPosition)
-    .attr("y", height / 2)
-    .merge(texts)
-    .text((d) => d.type);
-  texts.exit().attr("r", 0).remove();
+    .merge(groups.select("text"))
+    .text((d) => d.type)
+    .attr("fill", (d) => colorScale(d.type));
 };
