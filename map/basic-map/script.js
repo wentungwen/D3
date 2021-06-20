f3893cbcd8f99d99e7db8622ef8ee56de882d105
wentungwen <wentungwen@gmail.com>
2021-06-20 19:34:05 +0800

    map interaction

--- map/basic-map/script.js
+++ map/basic-map/script.js
@@ -2,21 +2,30 @@ const svg = d3.select("svg");
 const width = +svg.attr("width");
 const height = +svg.attr("height");
 
-const projection = d3.geoOrthographic();
-// console.log(countries.features);
+const projection = d3.geoConicEqualArea();
 const pathGenerator = d3.geoPath().projection(projection);
 
-d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(
-  (data) => {
-    const countries = topojson.feature(data, data.objects.countries);
-    console.log(countries.features);
-    console.log(countries);
-
-    svg
-      .selectAll("path")
-      .data(countries.features)
-      .enter()
-      .append("path")
-      .attr("d", (d) => pathGenerator(d));
-  }
+d3.tsv("https://unpkg.com/world-atlas@1.1.4/world/110m.tsv").then((data) =>
+  console.log(data)
 );
+
+svg
+  .append("path")
+  .attr("class", "sphere")
+  .attr("d", pathGenerator({ type: "Sphere" }));
+
+d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json").then((data) => {
+  const countries = topojson.feature(data, data.objects.countries);
+  // console.log(countries.features);
+  // console.log(countries);
+
+  svg
+    .selectAll("path")
+    .data(countries.features)
+    .enter()
+    .append("path")
+    .attr("d", (d) => pathGenerator(d))
+    .attr("class", "country")
+    .append("title")
+    .text((d) => console.log(1));
+});
